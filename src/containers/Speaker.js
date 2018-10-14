27d5eaf7abe15dd68f7621fba3d8042f40ad7e9d
Nate Taylor <taylonr@gmail.com>
2018-10-14 11:20:45 -0500

    Initial stab at speaker page.

--- src/containers/Speaker.js
+++ src/containers/Speaker.js
@@ -1,9 +1,21 @@
 import React from 'react'
 import { withRouteData } from 'react-static'
-import { Image } from 'rebass'
+import { Box, Flex, Image, Text } from 'rebass'
 
-export default withRouteData(({ speaker }) => <div>
-        <h1>{speaker.name}</h1>
-        <h2>{speaker.title}</h2>
-        <Image src={speaker.photo} />
-    </div>)
+export default withRouteData(({ speaker }) => (
+  <Box mb={8} p={4}>
+    <Flex flexWrap="nowrap" alignItems="space-between">
+      <Image src={speaker.photo} />
+      <Flex flexDirection="column" px={4}>
+        <Flex flexDirection="column">
+          <h1>{speaker.title}</h1>
+          <Text fontSize={[2, 3]} pb={3}>{speaker.description}</Text>
+        </Flex>
+      </Flex>
+    </Flex>
+    <Flex flexDirection="column">
+      <h1>{speaker.firstName} {speaker.lastName}</h1>
+      <Text fontSize={[2, 3]}>{speaker.bio}</Text>
+    </Flex>
+  </Box>
+))
--- src/theme.js
+++ src/theme.js
@@ -7,6 +7,7 @@ export default {
     dark: '#58595b',
     darkblue: '#0d1d42',
   },
+  fontSizes: [12, 14, 16, 20, 24, 32, 48, 64],
   space: ['0rem', '0.236rem', '0.382rem', '0.618rem', '1rem', '1.618rem', '2.618rem', '4.236rem', '6.854rem'],
   borders: ['0.0625rem solid'],
 }
