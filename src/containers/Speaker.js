88a565f68b707beef3d5847d89eb7fe6c4369b2c
Nate Taylor <taylonr@gmail.com>
2018-10-14 12:34:10 -0500

    Better aligning text & photo

--- src/containers/Speaker.js
+++ src/containers/Speaker.js
@@ -3,19 +3,23 @@ import { withRouteData } from 'react-static'
 import { Box, Flex, Image, Text } from 'rebass'
 
 export default withRouteData(({ speaker }) => (
-  <Box mb={8} p={4}>
-    <Flex flexWrap="nowrap" alignItems="space-between">
-      <Image src={speaker.photo} />
-      <Flex flexDirection="column" px={4}>
+  <Flex mx={[3, 4, 5]}>
+    <Box width={[1, 1 / 3]}>
+      <Image src={speaker.photo} mt={3} />
+    </Box>
+    <Box width={[1, 2 / 3]}>
+      <Flex flexWrap="nowrap" alignItems="space-between">
         <Flex flexDirection="column">
-          <h1>{speaker.title}</h1>
-          <Text fontSize={[2, 3]} pb={3}>{speaker.description}</Text>
+          <Flex flexDirection="column">
+            <Text fontSize={5} pb={3} >{speaker.title}</Text>
+            <Text fontSize={[2, 3]} pb={3}>{speaker.description}</Text>
+          </Flex>
+          <Flex flexDirection="column">
+            <Text fontSize={5} pb={3}>{speaker.firstName} {speaker.lastName}</Text>
+            <Text fontSize={[2, 3]}>{speaker.bio}</Text>
+          </Flex>
         </Flex>
       </Flex>
-    </Flex>
-    <Flex flexDirection="column">
-      <h1>{speaker.firstName} {speaker.lastName}</h1>
-      <Text fontSize={[2, 3]}>{speaker.bio}</Text>
-    </Flex>
-  </Box>
+    </Box>
+  </Flex>
 ))
