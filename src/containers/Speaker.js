eb47b0b3a971a4b5b0a4e0d8047c407ad3bde8d3
Nate Taylor <taylonr@gmail.com>
2018-11-06 07:17:30 -0600

    Adding in information for round 2

--- src/containers/Speaker.js
+++ src/containers/Speaker.js
@@ -13,35 +13,40 @@ const RoundedButton = styled(Button)`
   cursor: pointer;
   border-radius: 50%;
 `
-export default withRouteData(({ speaker, nextId, previousId }) => (
-  <Flex mx={[3, 4, 5]} flexWrap="wrap">
-    <Box width={[1, 1 / 3]}>
-      <RoundedImage src={speaker.photo} pt={4} mt={3} />
-    </Box>
-    <Box width={[1, 2 / 3]} px={5}>
-      <Flex flexWrap="nowrap" alignItems="space-between">
-        <Flex flexDirection="column">
+export default withRouteData(({ speaker, nextId, previousId }) => {
+  const org = speaker.organization ? `/ ${speaker.organization}` : ''
+
+  return (
+    <Flex mx={[3, 4, 5]} flexWrap="wrap">
+      <Box width={[1, 1 / 3]}>
+        <RoundedImage src={speaker.photo} pt={4} mt={3} />
+      </Box>
+      <Box width={[1, 2 / 3]} px={5}>
+        <Flex flexWrap="nowrap" alignItems="space-between">
           <Flex flexDirection="column">
-            <Text fontSize={1} fontWeight={900} pt={4} pb={4}>Speaker</Text>
-            <Text fontSize={5} pb={7}>{speaker.firstName} {speaker.lastName} / {speaker.organization}</Text>
-            <Text fontSize={1} fontWeight={900} pb={4}>Session</Text>
-            <Text fontSize={3} pb={2} >{speaker.title}</Text>
-            <Text fontSize={[1, 2]} pb={7} fontWeight={300}>{speaker.description}</Text>
-          </Flex>
-          <Flex flexDirection="column">
-            <Text fontSize={1} fontWeight={900} pb={4}>Bio</Text>
-            <Text fontSize={[1, 2]} pb={6} fontWeight={300}>{speaker.bio}</Text>
+            <Flex flexDirection="column">
+              <Text fontSize={1} fontWeight={900} pt={4} pb={4}>Speaker</Text>
+              <Text fontSize={5} pb={7}>{speaker.firstName} {speaker.lastName} {org}</Text>
+              <Text fontSize={1} fontWeight={900} pb={4}>Session</Text>
+              <Text fontSize={3} pb={2} >{speaker.title}</Text>
+              <Text fontSize={[1, 2]} pb={7} fontWeight={300}>{speaker.description}</Text>
+            </Flex>
+            <Flex flexDirection="column">
+              <Text fontSize={1} fontWeight={900} pb={4}>Bio</Text>
+              <Text fontSize={[1, 2]} pb={6} fontWeight={300}>{speaker.bio}</Text>
+            </Flex>
           </Flex>
         </Flex>
-      </Flex>
-      <Flex alignItems="space-between">
-        <RoundedButton fontSize={3} p={5} bg="transparent" color="light" mr={4} onClick={() => { window.location = `/speakers/${previousId}` }}>
-          <NavigateBefore size={32} />
-        </RoundedButton>
-        <RoundedButton fontSize={3} p={5} bg="transparent" color="light" ml="auto" onClick={() => { window.location = `/speakers/${nextId}` }}>
-          <NavigateNext size={32} />
-        </RoundedButton>
-      </Flex>
-    </Box>
-  </Flex>
-))
+        <Flex alignItems="space-between">
+          <RoundedButton fontSize={3} p={5} bg="transparent" color="light" mr={4} onClick={() => { window.location = `/speakers/${previousId}` }}>
+            <NavigateBefore size={32} />
+          </RoundedButton>
+          <RoundedButton fontSize={3} p={5} bg="transparent" color="light" ml="auto" onClick={() => { window.location = `/speakers/${nextId}` }}>
+            <NavigateNext size={32} />
+          </RoundedButton>
+        </Flex>
+      </Box>
+    </Flex>
+  )
+}
+)
