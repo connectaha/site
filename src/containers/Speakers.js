123f9e1a39fab5f090dc26316421adbd1c8a13a5
Nate Taylor <taylonr@gmail.com>
2019-01-09 12:12:04 -0600

    If a speaker's picture isn't present, don't show it.

--- src/containers/Speakers.js
+++ src/containers/Speakers.js
@@ -8,36 +8,40 @@ const Clickable = styled.div`
   cursor: pointer;
 `
 
-export default withRouteData(({ speakers }) => 
-  // const shuffled = shuffle(speakers)
-   (
-
+export default withRouteData(({ speakers }) =>
+  (
     <div>
       <Heading pl={4} pt={6} pb={4} color="white">Speakers</Heading>
       <Flex flexWrap="wrap" p={32}>
         {
-        speakers.map(speaker => (
-          <Clickable key={speaker.id}>
-            <Box width={280} height={440}>
-              <Card
-                m={16}
-                p={2}
-                width={[1, 1, 1 / 2]}
-                borderRadius={8}
-                boxShadow="0 0 16px rgba(0, 0, 0, .25)"
-                onClick={() => { window.location = `/speakers/${speaker.id}` }}>
-                <Image src={speaker.photo} />
-                <Box px={2}>
-                  <Text color="black" fontSize={4}>
-                    {speaker.firstName}
-                  </Text>
-                  <Text color="black" fontSize={4}>
-                    {speaker.lastName}
-                  </Text>
-                </Box>
-              </Card>
-            </Box>
-          </Clickable>))
+        speakers.map(speaker => {
+          if (speaker.photo.length === 0) {
+            return null
+          }
+
+          return (
+            <Clickable key={speaker.id}>
+              <Box width={280} height={440}>
+                <Card
+                  m={16}
+                  p={2}
+                  width={[1, 1, 1 / 2]}
+                  borderRadius={8}
+                  boxShadow="0 0 16px rgba(0, 0, 0, .25)"
+                  onClick={() => { window.location = `/speakers/${speaker.id}` }}>
+                  <Image src={speaker.photo} />
+                  <Box px={2}>
+                    <Text color="black" fontSize={4}>
+                      {speaker.firstName}
+                    </Text>
+                    <Text color="black" fontSize={4}>
+                      {speaker.lastName}
+                    </Text>
+                  </Box>
+                </Card>
+              </Box>
+            </Clickable>)
+        })
       }
       </Flex>
     </div>)
