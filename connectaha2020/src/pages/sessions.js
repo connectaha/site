dcb78cd9f8f9ac3f557513c6a204d39fb7c8f188
Brian Pope <pope.brian@gmail.com>
2020-05-14 17:01:55 -0500

    Added Sessions

--- connectaha2020/src/pages/sessions.js
+++ connectaha2020/src/pages/sessions.js
@@ -1,12 +1,40 @@
 import React from "react"
+import { Link, graphql, useStaticQuery } from 'gatsby'
+import sessionsStyles from './sessions.module.scss'
 
 import Layout from '../components/layout'
 
 const SessionsPage = () => {
+    const data = useStaticQuery(graphql`
+        query {
+            allContentfulSpeakers ( sort: { fields: name, order: ASC } ) {
+                edges {
+                    node {
+                        name
+                        slug
+                        company
+                        session
+                    }
+                }
+            }
+        }
+    `)
+
     return (
         <Layout>
-            <h1>2021 Sessions</h1>
-            <p>Coming in January 2021</p>
+            <h1>Sessions</h1>
+            <ol className={sessionsStyles.sessions}>
+                {data.allContentfulSpeakers.edges.map((edge) => {
+                    return (
+                        <li className={sessionsStyles.session}>
+                            <Link to={`/speakers/${edge.node.slug}`}>
+                                <h2>{edge.node.session}</h2>
+                                <p>{edge.node.name} | {edge.node.company}</p>
+                            </Link>
+                        </li>
+                    )
+                })}
+            </ol>
         </Layout>
     )
 }
