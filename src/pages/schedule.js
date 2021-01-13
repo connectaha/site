7448f8c62686a25c13f652fb0781dfbb578eb968
Brian Pope <pope.brian@gmail.com>
2021-01-13 11:55:09 -0600

    updated Schedule

--- src/pages/schedule.js
+++ src/pages/schedule.js
@@ -1,8 +1,8 @@
 import React from "react"
+import scheduleStyles from './schedule.module.scss'
 
 import Layout from '../components/layout'
 import Head from '../components/head'
-import scheduleStyles from './schedule.module.scss'
 
 const SchedulePage = () => {
     return (
@@ -18,32 +18,32 @@ const SchedulePage = () => {
             <h4>10:05 A.M. - 10:35 A.M.</h4>
             <hr className={scheduleStyles.divider} />
             <h2>Hilary Stohs-Krause</h2>
-            <h3>How I got out of my own way and found the happy path in tech</h3>
+            <h3 className={scheduleStyles.title}>How I got out of my own way and found the happy path in tech</h3>
             <h4>10:40 A.M. - 11:10 A.M.</h4>
             <hr className={scheduleStyles.divider} />
 
             <h2>Ash Banaszek</h2>
-            <h3>A designer's guide to development</h3>
+            <h3 className={scheduleStyles.h3}>A designer's guide to development</h3>
             
             <h4>11:15 A.M. - 11:45 A.M.</h4>
             <hr className={scheduleStyles.divider} />
             <h2>Shonna Dorsey</h2>
-            <h3>Finding Your Place When You Don’t Know Where You Fit</h3>
+            <h3 className={scheduleStyles.title}>Finding Your Place When You Don’t Know Where You Fit</h3>
             
             <h4>11:50 A.M. - 12:20 P.M.</h4>
             <hr className={scheduleStyles.divider} />
             <h2>Amanda Perkins</h2>
-            <h3>Travels through the Looking Glass: My journey from Customer Service rep to SR SQA</h3>
+            <h3 className={scheduleStyles.title}>Travels through the Looking Glass: My journey from Customer Service rep to SR SQA</h3>
             
             <h4>12:25 P.M. - 12:55 P.M.</h4>
             <hr className={scheduleStyles.divider} />
             <h2>Kathy Andersen</h2>
-            <h3>How I Learned to Stop Worrying and Love the Process</h3>
+            <h3 className={scheduleStyles.title}>How I Learned to Stop Worrying and Love the Process</h3>
             
             <h4>1:00 P.M. - 1:30 P.M.</h4>
             <hr className={scheduleStyles.divider} />
             <h2>Sanjay Batra</h2>
-            <h3></h3>
+            <h3 className={scheduleStyles.title}></h3>
 
             <h4>1:30 P.M. - 1:35 P.M.</h4>
             <hr className={scheduleStyles.divider} />
