4fff40a9b8119ea9f269a1d8477136ccd027a4e0
Nate Tyalor <taylonr@gmail.com>
2022-04-03 18:29:10 -0500

    Be able to toggle abstract

---
+++ src/components/speaker.js
@@ -0,0 +1,32 @@
+import React, { useState } from "react"
+import Img from "gatsby-image"
+
+import speakersStyles from "../pages/speakers.module.scss"
+const Speaker = ({ edge }) => {
+  const [visible, setVisible] = useState(false)
+  return (
+    <div>
+      <div
+        className={speakersStyles.speaker}
+        key={edge.node.slug}
+        onClick={() => setVisible(!visible)}
+      >
+        {edge.node.image && <Img fluid={edge.node.image.fluid} />}
+        <div className="info">
+          <h3>{edge.node.name}</h3>
+          <h3>{edge.node.company}</h3>
+          <h4>{edge.node.session}</h4>
+        </div>
+      </div>
+      <div className={visible ? "" : speakersStyles.hidden}>
+        {edge.node.abstract.json.content.map(con => {
+          return con.content.map(c => {
+            return <p>{c.value}</p>
+          })
+        })}
+      </div>
+    </div>
+  )
+}
+
+export default Speaker
