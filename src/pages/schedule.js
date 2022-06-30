a98377f7f8205075232c1203295b381e567091ed
Brian Pope <brian@connectaha.com>
2022-06-30 10:58:13 -0500

    Updated Schedule

--- src/pages/schedule.js
+++ src/pages/schedule.js
@@ -21,7 +21,7 @@ const SchedulePage = () => {
                 <h4>9:00 A.M. - 9:45 A.M.</h4>
                 <hr className={scheduleStyles.divider} />
                 <h2>Kelsey Janda</h2>
-                <h3 className={scheduleStyles.talktitle}>TBD</h3>
+                <h3 className={scheduleStyles.talktitle}>Learning by Making: happier product team & customers</h3>
             </div>
 
             <div className={scheduleStyles.card}>
@@ -33,7 +33,7 @@ const SchedulePage = () => {
                 <h4>10:00 A.M. - 10:45 A.M.</h4>
                 <hr className={scheduleStyles.divider} />
                 <h2>Erica Cuttitta</h2>
-                <h3 className={scheduleStyles.talktitle}>TBD</h3>
+                <h3 className={scheduleStyles.talktitle}>Systems, code and APIs…, but first, humans</h3>
             </div>
 
             <div className={scheduleStyles.card}>
@@ -45,7 +45,7 @@ const SchedulePage = () => {
                 <h4>11:00 A.M. - 11:45 A.M.</h4>
                 <hr className={scheduleStyles.divider} />
                 <h2>Nate Hales</h2>
-                <h3 className={scheduleStyles.talktitle}>TBD</h3>
+                <h3 className={scheduleStyles.talktitle}>Holistic Quality: More Than Just Testing</h3>
             </div>
             <div className={scheduleStyles.card}>
                 <h4>11:45 A.M. - 1:00 P.M.</h4>
@@ -56,7 +56,7 @@ const SchedulePage = () => {
                 <h4>1:00 P.M. - 1:45 P.M.</h4>
                 <hr className={scheduleStyles.divider} />
                 <h2>Rhia Dixon</h2>
-                <h3 className={scheduleStyles.talktitle}>TBD</h3>
+                <h3 className={scheduleStyles.talktitle}>It's All Relative: The Science of Debugging</h3>
             </div>
 
             <div className={scheduleStyles.card}>
@@ -68,7 +68,7 @@ const SchedulePage = () => {
                 <h4>2:00 P.M. - 2:45 P.M.</h4>
                 <hr className={scheduleStyles.divider} />
                 <h2>Eric Gerling</h2>
-                <h3 className={scheduleStyles.talktitle}>TBD</h3>
+                <h3 className={scheduleStyles.talktitle}>Lessons Learned from a Lifetime of “No”</h3>
             </div>            
             
             <div className={scheduleStyles.card}>
@@ -80,7 +80,7 @@ const SchedulePage = () => {
                 <h4>3:00 P.M. - 3:45 P.M.</h4>
                 <hr className={scheduleStyles.divider} />
                 <h2>Steve Chikwaya</h2>
-                <h3 className={scheduleStyles.talktitle}>TBD</h3>
+                <h3 className={scheduleStyles.talktitle}>The rise of the product minded engineer</h3>
             </div>
 
             <div className={scheduleStyles.card}>
@@ -92,7 +92,7 @@ const SchedulePage = () => {
                 <h4>4:00 P.M. - 4:35 P.M.</h4>
                 <hr className={scheduleStyles.divider} />
                 <h2 className={scheduleStyles.header}>Rebecca Stavick</h2>
-                <h3>The Future of Omaha Technology</h3>
+                <h3>The Omaha Tech Community: An Interactive Discussion</h3>
             </div>
             <div className={scheduleStyles.card}>
                 <h4>4:35 P.M. - 6:00 P.M.</h4>
